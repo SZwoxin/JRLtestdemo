b899477c1cf9c9f74103172c5b51280fed02be95
SZwoxin <469551994@qq.com>
2018-02-11 17:02:50 +0800

    更新

--- Page/login.py
+++ Page/login.py
@@ -1,16 +1,8 @@
 # -*- coding:utf-8 -*-
 # Aothor:Lin
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-import unittest, time
-
 
 def login(self):
     driver = self.driver
-    driver.maximize_window ()
     driver.find_element_by_id ( "name" ).clear ()
     driver.find_element_by_id ( "name" ).send_keys ( '15011228811' )
     driver.find_element_by_id ( "password" ).clear ()
