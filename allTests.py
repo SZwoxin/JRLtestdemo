1c7d049a6673798f6794724856e63da038b79a36
SZwoxin <469551994@qq.com>
2018-02-14 11:52:08 +0800

    更新

--- allTests.py
+++ allTests.py
@@ -1,8 +1,13 @@
 # -*- coding:utf-8 -*-
 # Aothor:Lin
-import unittest, os, time
-from Report import HTMLTestRunner
+import os, unittest, time, smtplib
+from Model import HTMLTestRunner
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.image import MIMEImage
 
+
+# 定义测试报告路径
 def suite():
     test_dir = os.path.join ( os.getcwd (), "Testcase" )
     # test_dir =path
@@ -15,10 +20,12 @@ def suite():
     return dir_case
 
 
+#生成测试报告时间
 def getNowTime():
     return time.strftime ( "%Y-%m-%d_%H-%M-%S", time.localtime ( time.time () ) )
 
 
+#定义测试报告存放路径与报告名称
 def runAutomation():
     filename = u"G:\\caolinlin\\zidonghua\\JRLtestdemo\\Report\\" + getNowTime () + "_TestReort.html"
     fp = file ( filename, 'wb' )
@@ -28,6 +35,19 @@ def runAutomation():
         description=u"用例执行情况："
     )
     runner.run ( suite () )
+
+
+def sentmail(file_new):
+    mail_from = '469551994@qq.com'
+    mail_to = '1243054080@qq.com'
+    f = open ( file_new, 'rb' )
+    mail_body = f.read ()
+    f.close ()
+    msg = MIMEText ( mail_body, _subtype='html', _charset='utf-8' )
+    msg = ['subject'] = u'自动化测试报告'
+    msg = ['date'] = time.strftime ( '%a, %d %b %Y %H:%M:%S %z' )
+    smtp = smtplib.SMTP ()
+    smtp.connect ( smtp.qq.com)
 
 
 if __name__ == '__main__':
