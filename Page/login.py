be435439687cf418c36d8a4f516af126207d25f6
SZwoxin <469551994@qq.com>
2018-02-23 17:52:12 +0800

    更新

--- Page/login.py
+++ Page/login.py
@@ -5,8 +5,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
-from Model.data import ExcelUtil
-import ddt
 
 
 def login(self, name, pswd):
